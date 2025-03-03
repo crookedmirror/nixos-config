0d95919fef10d7055795a40ad19de7ff03d7ded1
crookedmirror <crookedmirror@xyz>
2025-03-03 13:43:04 +0000

    added 'safe' mode spec to use lts kernel

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -2,5 +2,6 @@
 	imports = [
 		./git.nix
 		./bash.nix
+		./hyprland
 	];
 }
--- nixos/modules/kernel.nix
+++ nixos/modules/kernel.nix
@@ -1,3 +1,8 @@
-{ pkgs, ... }: {
-	boot.kernelPackages = pkgs.linuxPackages_cachyos;
+{ lib, pkgs, ... }: {
+	boot.kernelPackages = lib.mkOverride 99 pkgs.linuxPackages_cachyos;
+	
+	specialisation.safe.configuration = {
+		system.nixos.tags = [ "lts" ];
+		boot.kernelPackages = lib.mkOverride 98 pkgs.linuxPackages;
+	};
 }

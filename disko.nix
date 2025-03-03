063edd1ab645c95df99ddf2d8d3f19c0b7a2eb8e
Crooked Mirror <cookedmirror@ventoy>
2025-03-03 15:53:37 +0000

    disko setup

---
+++ disko.nix
@@ -0,0 +1,33 @@
+{
+  disko.devices = {
+    disk = {
+      my-disk = {
+        device = "/dev/nvme0n1p3";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "1G";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "btrfs";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}

8a880e6b4f53867f4540844d22b198a1b5a02c64
crookedmirror <crookedmirror@xyz>
2025-09-15 10:57:45 +0200

    feat: add amnezia cfg

--- hosts/dellvis/default.nix
+++ hosts/dellvis/default.nix
@@ -1,4 +1,10 @@
-{ config, inputs, pkgs, lib, ... }:
+{
+  config,
+  inputs,
+  pkgs,
+  lib,
+  ...
+}:
 {
   imports = [
 
@@ -8,31 +14,30 @@
     ../../config/hardware/intel.nix
     ../../config/hardware/ergohaven.nix
     ../../config/hardware/ni-audio-6.nix
-    ../../config/graphical  
-
+    ../../config/graphical
+    ../../config/amnezia.nix
     ../../users/crookedmirror
-  
+
     ./fs.nix
     ./net.nix
     inputs.mdatp.nixosModules.mdatp
   ];
   nixpkgs.hostPlatform = "x86_64-linux";
-  
+
   virtualisation.docker.rootless = {
     enable = true;
     setSocketVariable = true;
   };
-  services.intune.enable = true; 
-  services.mdatp.enable = true; 
+  services.intune.enable = true;
+  services.mdatp.enable = true;
   systemd.user.timers.intune-agent.wantedBy = [ "graphical-session.target" ];
   systemd.sockets.intune-daemon.wantedBy = [ "sockets.target" ];
   services.flatpak.enable = true;
   programs.direnv.enable = true;
 
-
   security.tpm2.enable = true;
-security.tpm2.pkcs11.enable = true;  # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
-security.tpm2.tctiEnvironment.enable = true;  # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
+  security.tpm2.pkcs11.enable = true; # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
+  security.tpm2.tctiEnvironment.enable = true; # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
 
   #services.ollama.enable = true;
   #services.ollama.loadModels = [ "devstral" ];
@@ -45,7 +50,7 @@ security.tpm2.tctiEnvironment.enable = true;  # TPM2TOOLS_TCTI and TPM2_PKCS11_T
   #    "rw"
   #    "uid=1000"
   #  ];
-  #};  
+  #};
   environment = {
     systemPackages = with pkgs; [
       vim
@@ -54,5 +59,5 @@ security.tpm2.tctiEnvironment.enable = true;  # TPM2TOOLS_TCTI and TPM2_PKCS11_T
       bigsteam
       nvidia-offload
     ];
-  }; 
+  };
 }

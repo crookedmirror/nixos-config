b29e10dd5586e0832144df6427fb9ca2e2c9d911
Developer <Developer@development.xyz>
2025-09-25 00:20:25 +0200

    fix: more cleaning

--- users/shared/security/ssh.nix
+++ users/shared/security/ssh.nix
@@ -1,20 +1,8 @@
-{ config, inputs, ... }:
+{ config, ... }:
 {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
     inherit (config.userSecrets.ssh) matchBlocks;
-    #matchBlocks = {
-    #  "*" = {
-    #    addKeysToAgent = "yes";
-    #    controlMaster = "auto";
-    #  };
-    #  "github.com" = {
-    #    hostname = "github.com";
-    #    user = "git";
-    #    identitiesOnly = true;
-    #    identityFile = "${inputs.self.outPath}/id_ed25519";
-    #  };
-    #};
   };
 }

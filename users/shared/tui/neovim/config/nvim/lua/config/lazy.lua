2fa7d3689e6767867d7a01366e59bd7c35d9eeca
Developer <Developer@development.xyz>
2025-10-14 19:49:56 +0200

    feat: neovim add color scheme

--- users/shared/tui/neovim/config/nvim/lua/config/lazy.lua
+++ users/shared/tui/neovim/config/nvim/lua/config/lazy.lua
@@ -25,11 +25,11 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- import your plugins
-    { import = "plugins" },
+    { import = "config.plugins" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "catppuccin" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })

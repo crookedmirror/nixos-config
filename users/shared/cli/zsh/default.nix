e1f270564ebd82ec3b44f9d91060e622704bac8c
Developer <Developer@development.xyz>
2025-10-31 14:57:00 +0100

    fix: flake bump

--- users/shared/cli/zsh/default.nix
+++ users/shared/cli/zsh/default.nix
@@ -13,7 +13,7 @@ let
   fshPlugin = {
     name = "zsh-fast-syntax-highlighting";
     src = pkgs.zsh-fast-syntax-highlighting;
-    file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
+    file = "share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh";
   };
   fshPluginTheme = pkgs.stdenvNoCC.mkDerivation {
     name = "fsh-theme";

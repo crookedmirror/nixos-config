3b35efbaf6c2b8341ed4206f959532ee40e620bf
Developer <Developer@development.xyz>
2025-12-10 13:04:52 +0100

    feat: add owt-marketplace

--- users/shared/tui/claude/default.nix
+++ users/shared/tui/claude/default.nix
@@ -1,4 +1,16 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   home.packages = with pkgs; [ claude-code ];
+
+  home.file.".claude/settings.json".text = builtins.toJSON {
+    alwaysThinkingEnabled = true;
+    extraKnownMarketplaces = {
+      "owt-marketplace" = {
+        source = {
+          source = "git";
+          url = "git@github.com:openwt/OWTAIMarketplace.git";
+        };
+      };
+    };
+  };
 }
